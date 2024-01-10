2465396e1d90b144e8a77b722331cba2e808338a
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2024-01-10 16:35:31 +0530

    Update errormiddleware.js

--- server/middleware/errormiddleware.js
+++ server/middleware/errormiddleware.js
@@ -1,8 +1,9 @@
 const errormiddleware = (err, req, res, next) => {
     const status = err.status || 500;
     const message = err || "Something went wrong";
+    const extradetails = "error from backend";
     console.log(message)
     // console.log(message,   err)
-    return res.status(status).json({  message });
+    return res.status(status).json({  message , extradetails });
 }
 module.exports = errormiddleware;
